7362bc07c47d890397cf9f687dfcd379e2277fdc
Hoainam25699 <hoainam25699@gmail.com>
2021-06-08 20:40:17 +0700

    count object noise cdt

--- PythonApp/PythonAppVenv/object_detection.py
+++ PythonApp/PythonAppVenv/object_detection.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 class CountObject:
   def __init__(self, image_path):
     self.image_path = image_path
+    self.new_image_path= image_path.replace('.jpg', "").replace('.png', '').replace('.jpeg','') + "_new.png"
+     
+
     self.kernel =  np.ones((3,3),np.uint8)
 
   def read_image(self):
@@ -21,7 +24,7 @@ class CountObject:
     axs[1].imshow(img, cmap="gray")
     axs[1].set_title("Total Count = {}".format(count))
     plt.show()
-  
+
   
   def show(self, image, name, waitKeyTime=2000):
     cv2.imshow(name, image)
@@ -44,33 +47,66 @@ class CountObject:
     # smoothing
     image_blur = cv2.medianBlur(image, 5)
     # transform to gray image
+    image_gray = cv2.cvtColor(image_blur, cv2.COLOR_BGR2GRAY)
     image_blur_gray = cv2.cvtColor(image_blur, cv2.COLOR_BGR2GRAY)
+
+    # self.show(image_gray, "image_gray")
+    # self.show( image_blur_gray, "image_Blur_gray")
 
     init = 108
     kernel = np.ones((3, 3), np.uint8)
 
     # use threshold to separate image into 2 parts 
-    image_res, image_thresh = cv2.threshold(image_blur_gray, init, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+    image_res, image_thresh = cv2.threshold(image_gray, init, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+    image_res_blur, image_thresh_blur = cv2.threshold(image_blur_gray, init, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+
+    # self.show(image_thresh, "image_thresh")
+    # self.show(image_thresh_blur, "image_thresh_blur")
 
     # filter noise
     opening = cv2.morphologyEx(image_thresh, cv2.MORPH_OPEN, kernel)
-    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    ret, last_image = cv2.threshold(dist_transform, 0.3 * dist_transform.max(), 255, 0)
-    last_image = np.uint8(last_image)
-    return last_image
+    closing = cv2.morphologyEx(image_thresh, cv2.MORPH_CLOSE, kernel)
+
+    # self.show(opening, "opening")
+    # self.show(closing, "closing")
+
+
+    img_erosion = cv2.erode(image_thresh, kernel, iterations=1)
+    img_dilation = cv2.dilate(image_thresh, kernel, iterations=1)
+
+    # self.show(img_erosion, "img_erosion")
+    self.show(img_dilation, "img_dilation")
+
+
+    # dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    # dist_transform = cv2.distanceTransform(img_dilation, cv2.DIST_L2, 5)
+
+    # ret, last_image = cv2.threshold(dist_transform, 0.3 * dist_transform.max(), 255, 0)
+    # last_image = np.uint8(last_image)
+    # self.show(last_image, "last_image")
+
+    # return last_image
+    return img_dilation
 
   def count(self, image, last_image):
-    cnts = cv2.findContours(last_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = cv2.findContours(last_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(last_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
 
     for (i, c) in enumerate(cnts):
       ((x, y), _) = cv2.minEnclosingCircle(c)
-      cv2.putText(image, "#{}".format(i + 1), (int(x) - 45, int(y) + 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 5)
+      # cv2.putText(image, "#{}".format(i + 1), (int(x) - 45, int(y) + 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 5)
+      # cv2.putText(image, (int(x) - 45, int(y) + 20),  cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 5)
       cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 
     # tis funtion disply the image which i have already describe above
     self.display(image, len(cnts))
+    # Filename
+
+  
+    cv2.imwrite(self.new_image_path, image)
+
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
     return 
@@ -83,18 +119,18 @@ class CountObject:
 count_object_1 = CountObject('test_1.png')
 count_object_1.count_obj()
 
-count_object_2 = CountObject('test_2.png')
-count_object_2.count_obj()
+# count_object_2 = CountObject('test_2.png')
+# count_object_2.count_obj()
 
-# count_object_3 = CountObject('test_3.png')
-# count_object_3.count_obj()
+count_object_3 = CountObject('test_3.png')
+count_object_3.count_obj()
 
-# count_object_4 = CountObject('test_4.png')
-# count_object_4.count_obj()
+count_object_4 = CountObject('test_4.png')
+count_object_4.count_obj()
 
 
-# count_object_5 = CountObject('photo2.jpg')
-# count_object_5.count_obj()
+count_object_5 = CountObject('photo2.jpg')
+count_object_5.count_obj()
 
 
 
